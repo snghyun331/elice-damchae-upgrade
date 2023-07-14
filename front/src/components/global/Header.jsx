6a0db2d61b76fe82c1d90aead70fe565427c03a3
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-14 22:12:24 +0900

    Setting : heroicons 세팅

--- front/src/components/global/Header.jsx
+++ front/src/components/global/Header.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { UserCircleIcon } from '@heroicons/react/24/solid'
+
+
 
 const Header = () => {
   return (
@@ -39,11 +42,11 @@ const Header = () => {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-8 md:p-0 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col md:p-0 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className="block py-2 pl-3 pr-4 font-semibold text-white rounded md:bg-transparent md:p-0 hover:text-blue-700"
                 aria-current="page"
               >
                 대나무숲
@@ -52,9 +55,17 @@ const Header = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 font-semibold text-white rounded md:bg-transparent md:p-0 hover:text-blue-700"
               >
                 내 스토리
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 font-semibold text-white rounded md:bg-transparent md:p-0 hover:text-blue-700"
+              >
+                <UserCircleIcon className="h-6 w-6" />
               </a>
             </li>
           </ul>
