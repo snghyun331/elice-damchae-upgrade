baf916dea14a449fd18b99658c6cc3969934882a
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-17 13:01:20 +0900

    Feat :  스토리 쓰기 버튼 및 모달 여닫음 동작

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -1,9 +1,46 @@
+import { useState } from 'react';
+import StoryCreateModal from './StoryCreateModal'; // Modal 파일 경로에 맞게 수정
+
 const Home = () => {
-    return (
-        <div>
-            홈페이지 영역
-        </div>
-    )
-}
+  const [showStoryCreateModal, setShowStoryCreateModal] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowStoryCreateModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowStoryCreateModal(false);
+  };
+
+  return (
+    <div>
+      홈페이지 영역
+      <br />
+      <button
+        type="button"
+        onClick={handleButtonClick}
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        스토리 쓰기
+      </button>
+
+      {/* 모달 렌더링 */}
+      {showStoryCreateModal && (
+        <>
+          <StoryCreateModal
+            showStoryCreateModal={showStoryCreateModal}
+            handleModalClose={handleModalClose}
+          />
+          <button
+            type="button"
+            onClick={handleModalClose}
+            className="fixed inset-0 w-full h-full bg-black opacity-60 cursor-default"
+            aria-hidden="true"
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Home;
--- front/src/components/home/StoryCreateModal.jsx
+++ front/src/components/home/StoryCreateModal.jsx
@@ -1,7 +1,10 @@
-import moment from "moment";
+import moment from "moment"; // Import moment.js
 
-const StoryCreateModal = () => {
+// eslint-disable-next-line react/prop-types
+const StoryCreateModal = ({ showStoryCreateModal, handleModalClose }) => {
   const currentDate = moment().format("YYYY년 M월 D일"); // Format the current date
+
+  const modalStyle = showStoryCreateModal ? "" : "hidden";
 
   return (
     <div
@@ -9,7 +12,7 @@ const StoryCreateModal = () => {
       data-modal-backdrop="static"
       tabIndex="-1"
       aria-hidden="true"
-      className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className={`fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full ${modalStyle}`}
     >
       {/* 모달 내용 */}
       <div className="relative w-full max-w-2xl max-h-full">
@@ -22,6 +25,7 @@ const StoryCreateModal = () => {
             </h3>
             <button
               type="button"
+              onClick={handleModalClose}
               className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-hide="staticModal"
             >
@@ -44,35 +48,23 @@ const StoryCreateModal = () => {
             </button>
           </div>
           {/* 모달 본문 */}
-          <div className="p-6 space-y-6">
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new
-              consumer privacy laws for its citizens, companies around the world
-              are updating their terms of service agreements to comply.
-            </p>
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.)
-              goes into effect on May 25 and is meant to ensure a common set of
-              data rights in the European Union. It requires organizations to
-              notify users as soon as possible of high-risk data breaches that
-              could personally affect them.
-            </p>
-          </div>
+          <div className="p-6 space-y-6">{/* ... */}</div>
           {/* 모달 푸터 */}
           <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
             <button
               data-modal-hide="staticModal"
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              I accept
+              작성 완료
             </button>
             <button
+              onClick={handleModalClose}
               data-modal-hide="staticModal"
               type="button"
-              className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-md border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
             >
-              Decline
+              닫기
             </button>
           </div>
         </div>
