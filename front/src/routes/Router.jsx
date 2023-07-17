15abd1b269478b36aa2212ed3615ef3de0d7db28
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-17 16:45:25 +0900

    Feat : 라우터 계층화

--- front/src/routes/Router.jsx
+++ front/src/routes/Router.jsx
@@ -2,13 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../components/home/Home';
 import MyPage from '../components/MyPage/MyPage';
 import Story from '../components/Story/Story';
+import LoginForm from '../components/User/Loginform';
+import RegisterForm from '../components/User/Registerform';
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 
+			<Route path="/login" element={<LoginForm />} />
+
+			<Route path="/register" element={<RegisterForm />} />
+
 			<Route path="/mypage" element={<MyPage />} />
+
+			<Route path="/infochange" element={<MyPage />} />
 
 			<Route path="/stories" element={<Story />}>
 				<Route path=":index" element={<Story />} />
@@ -22,7 +30,6 @@ const Router = () => {
 				<Route path="write" element={<Story />} />
 			</Route>
 
-			<Route path="/infochange" element={<MyPage />} />
 		</Routes>
 	);
 };
