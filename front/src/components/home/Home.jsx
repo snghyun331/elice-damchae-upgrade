ba2841e5c89c5eca95ee71a861e32f3a5e97b06e
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-17 19:42:55 +0900

    Feat : 홈 배치항목 형태 정리

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -21,22 +21,27 @@ const Home = () => {
 		<div>
 			<BannerCarousel />
 			<br />
-			사용자이름님, 행복한 하루 보내세요.
-			<div>
-				<button
-					onClick={() => navigate('/stories')}
-					type="button"
-					className="w-36 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-				>
-					내 스토리
-				</button>
-				<button
-					onClick={handleButtonClick}
-					type="button"
-					className="w-36 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-				>
-					스토리 쓰기
-				</button>
+			<div className="mt-16 mb-16 flex justify-center items-center flex-col">
+				<span className="text-2xl">대머리독수리 님, 행복한 하루 보내세요.</span>
+				<br />
+				<div className="mb-10">
+					<button
+						onClick={() => navigate('/stories')}
+						type="button"
+						className="w-36 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-sm text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+					>
+						내 스토리
+					</button>
+					<button
+						onClick={handleButtonClick}
+						type="button"
+						className="w-36 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-sm text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+					>
+						스토리 쓰기
+					</button>
+				</div>
+			</div>
+			<div className="items-center">
 				<Search />
 			</div>
 			<div>
