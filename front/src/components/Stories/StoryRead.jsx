6afceec9261ee1a04112ef1dc12ef8b2ad1c8098
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-18 13:18:34 +0900

    Design : 개별 스토리 읽기 기본 구조

--- front/src/components/Stories/StoryRead.jsx
+++ front/src/components/Stories/StoryRead.jsx
@@ -27,47 +27,49 @@ const StoryRead = () => {
 					<div className="absolute inset-0 bg-black opacity-60"></div>
 
 					<div className="absolute top-1 left-1 p-4 z-10 max-w-md">
-						<div>
-							<p className="text-white mb-1">{dummyData.createdAt}</p>
-							<h5 className="leading-loose text-white text-2xl font-bold">
-								{dummyData.title}
-							</h5>
-						</div>
+						<p className="text-white mb-1">{dummyData.createdAt}</p>
+						<h5 className="leading-loose text-white text-2xl font-bold">
+							{dummyData.title}
+						</h5>
 					</div>
-					<div className="text-sm text-end absolute top-0 right-0 m-4">
+					<div className="text-sm text-end absolute top-1 right-1 m-4">
 						<p className="text-white mb-1">조회 123</p>
-						<button className="text-white underline underline-offset-2 text-red-400">삭제</button>
+						<button className="text-white underline underline-offset-2 text-red-400">
+							삭제
+						</button>
 					</div>
 				</div>
 
-				<div className="flex items-center justify-center h-16">
-					<span className="text-6xl">😊</span>
-				</div>
-				<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-					Here are the biggest enterprise technology acquisitions of 2021 so
-					far, in reverse chronological order.
-				</p>
-				<a
-					href="#"
-					className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>
-					Read more
-					<svg
-						className="w-3.5 h-3.5 ml-2"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 14 10"
+				<div>
+					<div className="relative -top-20 left-1 p-4 max-w-md">
+						<span className="text-9xl">😃</span>
+					</div>
+					<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+						Here are the biggest enterprise technology acquisitions of 2021 so
+						far, in reverse chronological order.
+					</p>
+					<a
+						href="#"
+						className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 					>
-						<path
-							stroke="currentColor"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M1 5h12m0 0L9 1m4 4L9 9"
-						/>
-					</svg>
-				</a>
+						Read more
+						<svg
+							className="w-3.5 h-3.5 ml-2"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 14 10"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M1 5h12m0 0L9 1m4 4L9 9"
+							/>
+						</svg>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
