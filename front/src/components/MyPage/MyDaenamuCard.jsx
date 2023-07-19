20f3c8293f5b27877f2efffad777e7a9e92fde3e
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-19 18:25:27 +0900

    Feat : 내가 쓴 대나무숲 카드 추가

--- front/src/components/MyPage/MyDaenamuCard.jsx
+++ front/src/components/MyPage/MyDaenamuCard.jsx
@@ -3,9 +3,10 @@ import { ChevronRightIcon } from '@heroicons/react/24/solid';
 
 const MyDaenamuCard = ({ data }) => {
 	return (
+
 		<a
 			href="#"
-			className="block max-w-sm p-6 bg-white border border-gray-200 rounded-2xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+			className="h-40 block max-w-sm p-6 bg-white border border-gray-200 rounded-2xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
 		>
 			<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 				{data.title}
@@ -18,6 +19,7 @@ const MyDaenamuCard = ({ data }) => {
 				<span className="ml-1">더보기</span>
 			</div>
 		</a>
+
 	);
 };
 
