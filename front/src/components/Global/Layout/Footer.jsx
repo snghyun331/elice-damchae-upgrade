946ef6fefbd8f1cddbf9e85579757931a2233e31
[AI7/2반]이지윤 <little8867@gmail.com>
2023-07-19 16:45:49 +0900

    feat:폴더대소문자변경

---
+++ front/src/components/Global/Layout/Footer.jsx
@@ -0,0 +1,29 @@
+const Footer = () => {
+	return (
+		<>
+			<footer className="bg-blue-400 dark:bg-gray-900">
+				<div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+					<div className="sm:flex sm:items-center sm:justify-between">
+						<a
+							href="https://flowbite.com/"
+							className="flex items-center mb-4 sm:mb-0"
+						>
+							<span className="text-white self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+								Damchae
+							</span>
+						</a>
+						<span className="text-white text-sm text-gray-500 sm:text-center dark:text-gray-400">
+							© 2023{' '}
+							<a href="https://flowbite.com/" className="hover:underline">
+								Flowbite™
+							</a>
+							. All Rights Reserved.
+						</span>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
