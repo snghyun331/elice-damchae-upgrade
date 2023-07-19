b22fb3fb7cf22210615de4baac1aa8d5a3aa211d
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-19 17:35:00 +0900

    최신 진행상황 복구

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,6 @@
-import cors from "cors";
-import express from "express";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import cors from 'cors';
+import express from 'express';
+import { errorMiddleware } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // 기본 페이지
-app.get("/", (req, res) => {
-  res.send("안녕하세요, 레이서 프로젝트 API 입니다.");
+app.get('/', (req, res) => {
+	res.send('안녕하세요, 레이서 프로젝트 API 입니다.');
 });
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import 'react-calendar/dist/Calendar.css';
+import './components/MyPage/MyCalendar.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
 );
