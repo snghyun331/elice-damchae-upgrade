bece594b8e52270c32657b59596b416391fefbc4
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-19 16:58:07 +0900

    마이페이지용 미니 대나무카드 생성

--- front/src/components/MyPage/MyDaenamus.jsx
+++ front/src/components/MyPage/MyDaenamus.jsx
@@ -1,3 +1,5 @@
+import MyDaenamuCard from './MyDaenamuCard';
+
 import { ChevronRightIcon } from '@heroicons/react/24/solid';
 
 const MyDaenamus = () => {
@@ -8,6 +10,7 @@ const MyDaenamus = () => {
 			<div className="text-base mt-4 mb-4 text-gray-700">
 				<ChevronRightIcon className="w-4 inline mb-1" />
 				<span className="inline">&nbsp;전체보기</span>
+				<MyDaenamuCard />
 			</div>
 		</>
 	);
