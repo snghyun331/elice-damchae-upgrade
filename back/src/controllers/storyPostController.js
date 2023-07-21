32d757b4eed73e5c866a4eb58db69d21720fae64
Lee SH <snghyun331@gmail.com>
2023-07-22 00:51:02 +0900

    Refact: storyPost 파일 관리

--- back/src/controllers/storyPostController.js
+++ back/src/controllers/storyPostController.js
@@ -1,19 +1,33 @@
 import { StoryPost } from '../db/schemas/storyPost.js';
+import { StoryPostService } from '../services/storyPostService.js';
 
 const storyPostController = {
 	createStoryPost: async (req, res, next) => {
 		try {
+			const { title, content, contentImg, storyImg, isPublic, mood, music } =
+				req.body;
 			const userId = req.currentUserId;
-			const storyPostInfo = await StoryPost.create({
-				userInfo: userId,
-				title: req.body.title,
-				content: req.body.content,
-				contentImg: req.body.contentImg,
-				storyImg: req.body.storyImg,
-				isPublic: req.body.isPublic,
-				mood: req.body.mood,
-				music: req.body.music,
+			const userInfo = userId;
+			const storyPostInfo = await StoryPostService.addStoryPost({
+				userInfo,
+				title,
+				content,
+				contentImg,
+				storyImg,
+				isPublic,
+				mood,
+				music,
 			});
+			// const storyPostInfo = await StoryPost.create({
+			// 	userInfo: userId,
+			// 	title: req.body.title,
+			// 	content: req.body.content,
+			// 	contentImg: req.body.contentImg,
+			// 	storyImg: req.body.storyImg,
+			// 	isPublic: req.body.isPublic,
+			// 	mood: req.body.mood,
+			// 	music: req.body.music,
+			// });
 			const result = await StoryPost.populate(storyPostInfo, {
 				path: 'userInfo',
 			});
