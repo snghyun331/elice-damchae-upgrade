059cfde493d477dba2f7bfeeb9eaa09961a19d5d
Lee SH <snghyun331@gmail.com>
2023-07-23 13:21:22 +0900

    Fix: timestamp 원래대로

--- back/src/db/schemas/user.js
+++ back/src/db/schemas/user.js
@@ -1,14 +1,5 @@
 import { Schema, model } from 'mongoose';
 
-// 1. UTC를 한국 시간대로 변환하는 함수
-const getKoreanTimestamp = () => {
-	// 한국 시간대는 UTC+9
-	const koreanTimezoneOffset = 9 * 60;
-	const now = new Date();
-	now.setMinutes(now.getMinutes() + koreanTimezoneOffset);
-	return now;
-};
-
 const UserSchema = new Schema(
 	{
 		email: {
@@ -35,11 +26,7 @@ const UserSchema = new Schema(
 		},
 	},
 	{
-		timestamps: {
-			createdAt: 'createdAt', // 필드 이름을 createdAt으로 지정
-			updatedAt: 'updatedAt', // 필드 이름을 updatedAt으로 지정
-			currentTime: () => getKoreanTimestamp(), // 생성 시간과 업데이트 시간을 한국 시간대의 timestamp로 설정
-		},
+		timestamps: true,
 	},
 );
 
