58402ecbd3052ce85361705d2bc17a2fd56ecdd4
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-24 17:36:14 +0900

    Refac : InfoChange에 useUserStore 적용

--- front/src/components/User/InfoChange.jsx
+++ front/src/components/User/InfoChange.jsx
@@ -1,61 +1,57 @@
+import useUserStore from '../../store/useUserStore';
 import { useState, useCallback, useMemo } from 'react';
 import Select from 'react-select';
-import useUserStore from '../../store/useUserStore';
 import { mbtiList } from '../Util/Util';
 
-const InfoChange = () => {
-	const [user, setUser] = useState({
-		email: '',
-		password: '',
-		nickname: '',
-		mbti: '',
-		profileImg: '/images/loginimg.jpg',
-	});
+import { getApi } from '../../services/api';
 
+const InfoChange = () => {
 	const {
 		email,
 		nickname,
 		mbti,
 		profileImg,
 
-		setEmail,
 		setNickname,
 		setMbti,
 		setProfileImg,
 	} = useUserStore();
 
 	const [password, setPassword] = useState('');
-
 	const [confirmPassword, setConfirmPassword] = useState('');
 
-	const [isPasswordFocused, setIsPasswordFocused] = useState(false);
-	const [isNicknameFocused, setIsNicknameFocused] = useState(false);
-	const [isConfirmFocused, setIsConfirmFocused] = useState(false);
+	const [nicknameCheck, setNicknameCheck] = useState(false);
 
-	const handleSubmit = () => {
-		//수정하기 요청
-	};
-	const handleChangeInput = useCallback(
-		(e) => {
-			setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-		},
-		[setUser],
-	);
+	const [focusedMap, setFocusedMap] = useState({
+		email: false,
+		password: false,
+	});
 
-	const handleImageUpload = (event) => {
-		const file = event.target.files[0];
-		setUser((prev) => ({
-			...prev,
-			profileImg: URL.createObjectURL(file),
-		}));
+	const handleFocus = (name, value) => {
+		setFocusedMap({ ...focusedMap, [name]: value });
 	};
 
-	const handleChangeConfirm = useCallback(
-		(e) => {
-			setConfirmPassword(e.target.value);
-		},
-		[setConfirmPassword],
-	);
+	const handleChangeInput = useCallback((e) => {
+		const { name, value } = e.target;
+
+		switch (name) {
+			case 'password':
+				setPassword(value);
+				break;
+			case 'nickname':
+				setNickname(value);
+				setNicknameCheck(false);
+				break;
+			case 'confirmPassword':
+				setConfirmPassword(value);
+				break;
+			case 'profileImg': {
+				const file = e.target.files[0];
+				setProfileImg(URL.createObjectURL(file));
+				break;
+			}
+		}
+	}, []);
 
 	const validatePassword = () => {
 		const passwordRegex =
@@ -68,21 +64,38 @@ const InfoChange = () => {
 		return nicknameRegex.test(nickname);
 	};
 
-	const isPasswordValid = useMemo(validatePassword, [validatePassword]);
-	const isNicknameValid = useMemo(validateNickname, [validateNickname]);
+	const isPasswordValid = useMemo(validatePassword, [password]);
+	const isNicknameValid = useMemo(validateNickname, [nickname]);
 	const isPasswordSame = useMemo(
 		() => password === confirmPassword,
 		[password, confirmPassword],
 	);
 
 	const isFormValid = useMemo(
-		() => isPasswordValid && isPasswordSame && isNicknameValid && mbti,
-		[isPasswordValid, isPasswordSame, isNicknameValid, mbti],
+		() => isPasswordValid && isPasswordSame && isNicknameValid && nicknameCheck && mbti,
+		[isPasswordValid, isPasswordSame, isNicknameValid, nicknameCheck, mbti],
 	);
 
-	const handleNicknameCheck = () => {
-		// Logic for verification code verification
-		// You can implement your own verification code verification functionality here
+	const handleNicknameCheck = async () => {
+		try {
+			const response = await getApi(`auth/check-nickname?=`, nickname);
+			console.log(response.data);
+
+			if (response.data.state == 'usableNickname') {
+				alert(response.data.alertMsg);
+				setNicknameCheck(true);
+			}
+			if (response.data.state == 'unusableNickname') {
+				alert(response.data.alertMsg);
+				setNicknameCheck(false);
+			}
+		} catch (error) {
+			console.log(error.response.data.message);
+		}
+	};
+
+	const handleSubmit = () => {
+		//수정하기 요청
 	};
 
 	return (
@@ -105,17 +118,24 @@ const InfoChange = () => {
 									<input
 										className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
 										id="file_input"
+										name="profileImg"
 										type="file"
-										onChange={handleImageUpload}
+										onChange={handleChangeInput}
 									/>
 
 									<div className="mt-4">
-										<p className="text-sm text-gray-500">선택된 이미지:</p>
-										<img
-											className="mt-2 max-w-xs"
-											src={profileImg}
-											alt="Selected Thumbnail"
-										/>
+										{profileImg ? (
+											<>
+												<p className="text-sm text-gray-500">선택된 이미지:</p>
+												<img
+													className="mt-2 max-w-xs"
+													src={profileImg}
+													alt="Selected Thumbnail"
+												/>
+											</>
+										) : (
+											''
+										)}
 									</div>
 								</div>
 								<div className="flex flex-col">
@@ -147,13 +167,13 @@ const InfoChange = () => {
 										placeholder="••••••••"
 										className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 										required=""
-										onFocus={() => setIsPasswordFocused(true)}
-										onBlur={() => setIsPasswordFocused(false)}
+										onFocus={() => handleFocus('confirmPassword', true)}
+										onBlur={() => handleFocus('confirmPassword', false)}
 									/>
-									{!isPasswordValid && isPasswordFocused && (
+									{!isPasswordValid && focusedMap.password && (
 										<p className="text-red-500 text-xs italic">
-											비밀번호는 8~20자 영문, 숫자, 특수문자 조합으로 설정해
-											주세요.
+											비밀번호는 8~20자 영문, 숫자, 특수문자 조합으로
+											설정해주세요.
 										</p>
 									)}
 								</div>
@@ -166,17 +186,17 @@ const InfoChange = () => {
 									</label>
 									<input
 										value={confirmPassword}
-										onChange={handleChangeConfirm}
+										onChange={handleChangeInput}
 										type="password"
 										name="confirmPassword"
 										id="confirm-password"
 										placeholder="••••••••"
 										className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 										required=""
-										onFocus={() => setIsConfirmFocused(true)}
-										onBlur={() => setIsConfirmFocused(false)}
+										onFocus={() => handleFocus('confirmPassword', true)}
+										onBlur={() => handleFocus('confirmPassword', false)}
 									/>
-									{!isPasswordSame && isConfirmFocused && (
+									{!isPasswordSame && focusedMap.confirmPassword && (
 										<p className="text-red-500 text-xs italic">
 											비밀번호가 일치하지 않습니다.
 										</p>
@@ -195,8 +215,8 @@ const InfoChange = () => {
 										<input
 											value={nickname}
 											onChange={handleChangeInput}
-											onFocus={() => setIsNicknameFocused(true)}
-											onBlur={() => setIsNicknameFocused(false)}
+											onFocus={() => handleFocus(true)}
+											onBlur={() => handleFocus(false)}
 											type="text"
 											name="nickname"
 											id="nickname"
@@ -212,7 +232,7 @@ const InfoChange = () => {
 											중복 확인
 										</button>
 									</div>
-									{!isNicknameValid && isNicknameFocused && (
+									{!isNicknameValid && focusedMap.nickname && (
 										<p className="text-red-500 text-xs italic">
 											닉네임은 2~16자 사이로 설정해주세요.
 										</p>
@@ -227,10 +247,7 @@ const InfoChange = () => {
 										MBTI
 									</label>
 									<Select
-										value={mbti}
-										onChange={(selectedOption) =>
-											setUser((prev) => ({ ...prev, mbti: selectedOption }))
-										}
+										onChange={(selectedOption) => setMbti(selectedOption.value)}
 										options={mbtiList}
 										placeholder="Select MBTI"
 										classNamePrefix="react-select"
