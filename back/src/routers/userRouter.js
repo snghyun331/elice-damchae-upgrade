e2c678ed1d7b9673e5c7dc33c6bb6fd541d64522
[AI7}탁은경 <xkrdmsrud24@gmail.com>
2023-07-26 14:45:45 +0900

    fix : create-read-update-delete 로 통일

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -16,7 +16,7 @@ userAuthRouter.put('/users/:userId', userAuthController.userUpdate);
 // 닉네임 중복확인
 userAuthRouter.get('/auth/checkNickname', userAuthController.checkNickname);
 
-userAuthRouter.put('/auth/out', userAuthController.userWithdraw);
+userAuthRouter.put('/auth/out', userAuthController.userDelete);
 
 // userRouter.get('user/stories', async function (req, res, next) {});
 // userRouter.get('user/postLikes', async function (req, res, next) {});
