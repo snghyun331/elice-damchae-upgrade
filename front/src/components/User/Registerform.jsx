85d886023d8200d907456bab7fca84fc0090df9e
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-26 13:22:56 +0900

    fix : 최초 진입시에는 validation 에러 문구 뜨지 않고, input 발생했을 때만 뜨도록 수정

--- front/src/components/User/Registerform.jsx
+++ front/src/components/User/Registerform.jsx
@@ -175,7 +175,7 @@ const RegisterForm = () => {
 											이메일인증
 										</button>
 									</div>
-									{!isEmailValid && (
+									{!isEmailValid && email && (
 										<p className="text-red-500 text-xs italic">
 											이메일 형식이 올바르지 않습니다.
 										</p>
@@ -220,7 +220,7 @@ const RegisterForm = () => {
 										className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 										required=""
 									/>
-									{!isPasswordValid && (
+									{!isPasswordValid && password && (
 										<p className="text-red-500 text-xs italic">
 											비밀번호는 8~20자 영문, 숫자, 특수문자 조합으로
 											설정해주세요.
@@ -244,7 +244,7 @@ const RegisterForm = () => {
 										className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 										required=""
 									/>
-									{!isPasswordSame && (
+									{!isPasswordSame && confirmPassword && (
 										<p className="text-red-500 text-xs italic">
 											비밀번호가 일치하지 않습니다.
 										</p>
@@ -279,7 +279,7 @@ const RegisterForm = () => {
 											중복 확인
 										</button>
 									</div>
-									{!isNicknameValid && (
+									{!isNicknameValid && nickname && (
 										<p className="text-red-500 text-xs italic">
 											닉네임은 2~16자 사이로 설정해주세요.
 										</p>
