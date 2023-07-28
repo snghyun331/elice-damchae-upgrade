7c56299db5b119f08afc6afe3dd2ee5be6c67b01
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-07-28 17:50:36 +0900

    design : MBTI 필터 뱃지 추가

--- front/src/components/Daenamus/DaenamusMain.jsx
+++ front/src/components/Daenamus/DaenamusMain.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
 import Search from '../Global/Search';
 import DaenamusCard from '../Global/DaenamusCard';
-
+import { mbtiList } from '../Util/Util';
 import { Link } from 'react-router-dom';
 
 const DaenamusMain = () => {
 	const [showStoryCreateModal, setShowStoryCreateModal] = useState(false);
-
+	const [selectedMBTI, setSelectedMBTI] = useState([]);
+	console.log(selectedMBTI)
 	const handleButtonClick = () => {
 		setShowStoryCreateModal(true);
 	};
 
 	const handleModalClose = () => {
 		setShowStoryCreateModal(false);
+	};
+
+	const toggleMBTI = (value) => {
+		if (selectedMBTI.includes(value)) {
+			setSelectedMBTI(selectedMBTI.filter((item) => item !== value));
+		} else {
+			setSelectedMBTI([...selectedMBTI, value]);
+		}
 	};
 
 	return (
@@ -33,7 +42,6 @@ const DaenamusMain = () => {
 
 				<div className="flex justify-between mb-4">
 					<div>
-						{/* 버튼 대신 Link 컴포넌트를 사용하여 새로운 글쓰기 페이지로 이동하는 링크 생성 */}
 						<Link
 							to="/daenamus/write"
 							className="bg-blue-500 text-white px-4 py-2 rounded-md"
@@ -41,67 +49,29 @@ const DaenamusMain = () => {
 							글쓰기
 						</Link>
 					</div>
-
-					<ul className="flex space-x-4">
-						<li>
-							<button className="text-blue-500">전체글</button>
-						</li>
-						<li>
-							<button className="text-blue-500">인기글</button>
-						</li>
-						<li>
-							<button className="text-blue-500">enfp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">enfj</button>
-						</li>
-						<li>
-							<button className="text-blue-500">entp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">entj</button>
-						</li>
-						<li>
-							<button className="text-blue-500">estp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">estj</button>
-						</li>
-						<li>
-							<button className="text-blue-500">esfp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">esfj</button>
-						</li>
-						<li>
-							<button className="text-blue-500">infp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">infj</button>
-						</li>
-						<li>
-							<button className="text-blue-500">intp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">intj</button>
-						</li>
-						<li>
-							<button className="text-blue-500">istp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">istj</button>
-						</li>
-						<li>
-							<button className="text-blue-500">isfp</button>
-						</li>
-						<li>
-							<button className="text-blue-500">isfj</button>
-						</li>
-					</ul>
 				</div>
 
+				<ul className="">
+					{mbtiList.map((item, index) => {
+						const isSelected = selectedMBTI.includes(item.value);
+
+						return (
+							<span
+								key={index}
+								onClick={() => toggleMBTI(item.value)}
+								className={`cursor-pointer border pt-1 bg-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full ${
+									isSelected ? 'border-blue-600 text-blue-600' : 'border-gray-400 text-gray-400'
+								} dark:bg-white ${
+									isSelected ? 'dark:text-blue-300' : 'dark:text-gray-400'
+								}`}
+							>
+								{item.label} {isSelected && <span>×</span>}
+							</span> 
+						);
+					})}
+				</ul>
+
 				<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-					{/* 게시글 카드를 데이터에 맞게 반복해서 생성 */}
 					{Array.from({ length: 12 }).map((_, index) => (
 						<div key={index}>
 							<DaenamusCard />
