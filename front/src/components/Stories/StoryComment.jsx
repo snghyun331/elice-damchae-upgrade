706d0d2d28be766bf8e1561bf4115eae25a7ccc3
[AI7/2반]이지윤 <little8867@gmail.com>
2023-07-30 23:16:28 +0900

    fix:댓글등록비활성화

--- front/src/components/Stories/StoryComment.jsx
+++ front/src/components/Stories/StoryComment.jsx
@@ -67,7 +67,8 @@ const StoryComment = ({ storyId }) => {
 					value={comment}
 				/>
 				<button
-					className="h-8 w-[10%] my-5 text-sm font-medium text-gray-900 focus:outline-none bg-orange-300 rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+					disabled={!comment}
+					className="h-8 w-[10%] my-5 text-sm font-medium text-gray-900 focus:outline-none bg-blue-300 disabled:bg-neutral-300 rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
 					onClick={handleSubmit}
 				>
 					등록
