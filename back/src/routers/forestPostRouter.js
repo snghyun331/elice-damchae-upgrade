4805afbb378b5735a926ce5ef65fb69db5284733
Lee SH <snghyun331@gmail.com>
2023-07-30 11:09:27 +0900

    Fix: 대나무숲 글 등록 및 감정분석 api 구현

--- back/src/routers/forestPostRouter.js
+++ back/src/routers/forestPostRouter.js
@@ -3,20 +3,23 @@ import ForestController from '../controllers/forestController.js';
 import { loginRequired } from '../middlewares/loginRequired.js';
 const router = express.Router();
 
-// 게시글 단일 조회
-router.get('/search', ForestController.findByForest);
-
 // 글 등록
 router.post('/', loginRequired, ForestController.createPost);
 
-// 글 수정
-router.put('/:id', loginRequired, ForestController.updatePost);
+// 글 감정분석
+router.post('/senti-predict', loginRequired, ForestController.getPredict);
 
-// 글 삭제
-router.delete('/:id', loginRequired, ForestController.deletePost);
+// // 글 수정
+// router.put('/:id', loginRequired, ForestController.updatePost);
 
-router.get('/:forestId', ForestController.readStoryDetail);
+// // 글 삭제
+// router.delete('/:id', loginRequired, ForestController.deletePost);
 
-router.get('/', ForestController.readAllStories);
+// // 게시글 단일 조회
+// router.get('/search', ForestController.findByForest);
+
+// router.get('/:forestId', ForestController.readStoryDetail);
+
+// router.get('/', ForestController.readAllStories);
 
 export default router;
