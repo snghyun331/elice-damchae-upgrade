7d66f96e1a32eeac1078a27c861823a2232c4e9a
[AI7/2반]이지윤 <little8867@gmail.com>
2023-08-01 12:20:46 +0900

    fix:배포시map오류

--- front/src/components/Global/StoryCardMap.jsx
+++ front/src/components/Global/StoryCardMap.jsx
@@ -41,7 +41,7 @@ const StoryCardMap = () => {
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 text-base font-medium">
 					{isDataLoading ? (
 						<>
-							{stories.length > 0 ? ( // Check if stories is not empty
+							{stories && stories.length > 0 ? ( // Check if stories is not empty
 								<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 text-base font-medium">
 									{stories.map((story) => (
 										<div key={story._id}>
