2526671640471c7cf249f314e95c87c038f2c6aa
[AI7/2반]이지윤 <little8867@gmail.com>
2023-08-04 16:51:29 +0900

    feat:대나무숲삭제구현

--- front/src/components/Daenamus/DaenamuRead.jsx
+++ front/src/components/Daenamus/DaenamuRead.jsx
@@ -8,16 +8,21 @@ import {
 
 import { BackButton } from '../Global/BackButton';
 import { useNavigate, useParams } from 'react-router-dom';
-import { delApi, getApi } from '../../services/api';
+import { delApi, getApi, putApi } from '../../services/api';
 import { useEffect, useState } from 'react';
 import { useUserId } from '../../store/useUserStore';
 import StoryComment from '../Stories/StoryComment';
 import ReactionChart from './Reaction';
+import DaenamuTextEditor from './DaenamuTextEditor';
+import useStoryStore from '../../store/useStoryStore';
 
 const DaenamuRead = () => {
+	const { title, content, mood, setTitle, setContent, setMood } =
+		useStoryStore();
 	const { forestId } = useParams();
 	const [forest, setForest] = useState([]);
 	const [isDataLoading, setIsDataLoading] = useState(false);
+	const [editMode, setEditMode] = useState(false);
 	const navigate = useNavigate();
 	const id = useUserId();
 
@@ -41,6 +46,36 @@ const DaenamuRead = () => {
 		}
 	};
 
+	const handleEdit = () => {
+		setEditMode(true);
+		setTitle(forest.title);
+		setContent(forest.content);
+		setMood(forest.mood);
+	};
+
+	const handleCancelEdit = () => {
+		// Reset the edited comment and disable edit mode when canceling the edit
+		setTitle(forest.title);
+		setContent(forest.content);
+		setMood;
+		setEditMode(false);
+	};
+
+	const handleSaveEdit = async () => {
+		try {
+			// Call the onEdit function with the edited comment and the commentData._id
+			const res = await putApi(`forest/${forestId}`, {
+				title,
+				content,
+				mood,
+			});
+			console.log(res);
+			setEditMode(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	useEffect(() => {
 		fetchData();
 	}, []);
@@ -54,63 +89,98 @@ const DaenamuRead = () => {
 					backgroundColor: isDataLoading ? textToColor[forest.mood] : '#FFFFFF',
 				}}
 			>
-				<div className="relative h-40 overflow-hidden rounded-t-lg">
-					<div className="absolute w-full p-4">
-						<div className="flex flex-row items-center">
-							<div className="text-2xl">
-								{isDataLoading && textToIcon[forest.mood]}
-							</div>
-							<h5 className="p-5 leading-loose text-2xl font-bold">
-								{forest.title}
-							</h5>
-						</div>
-						<div className="w-full justify-between space-x-2 items-center inline-flex mr-3 text-sm text-gray-900 dark:text-white">
-							<div className="flex flex-row space-x-2">
-								<img
-									className="w-10 h-10 object-cover"
-									// src={isDataLoading && forest.userInfo.profileImg}
-									src={
-										'https://png.pngtree.com/png-vector/20191115/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_1990469.jpg'
-									}
-									alt=""
-								/>
-								<div className="flex flex-col ">
-									<p className="w-20 text-sm dark:text-gray-400">
-										{isDataLoading && forest.userInfo.nickname}
-									</p>
-									<p className="text-sm dark:text-gray-400">
-										{isDataLoading && forest.userInfo.mbti}
-									</p>
+				{' '}
+				<div className="justify-end">
+					{isDataLoading && forest.userInfo._id == id && (
+						<>
+							{editMode ? (
+								// Show Save and Cancel buttons in edit mode
+								<div className="mr-4 mt-4 flex flex-row justify-end">
+									<button
+										onClick={handleCancelEdit}
+										className="underline underline-offset-2 text-red-400"
+									>
+										취소
+									</button>
+									<button
+										onClick={handleSaveEdit}
+										className="ml-2 underline underline-offset-2 text-blue-400"
+									>
+										저장
+									</button>
+								</div>
+							) : (
+								// Show Edit button in view mode
+								<div className="mr-4 mt-4 flex flex-row justify-end">
+									<button
+										onClick={handleEdit}
+										className="justify-end underline underline-offset-2 text-red-400"
+									>
+										수정
+									</button>
+									<button
+										onClick={handleDelete}
+										className="justify-end ml-2 underline underline-offset-2 text-red-400"
+									>
+										삭제
+									</button>
+								</div>
+							)}
+						</>
+					)}
+				</div>
+				<div className="relative top-0 px-10">
+					{editMode ? (
+						<DaenamuTextEditor />
+					) : (
+						<div className="view-mode">
+							<div className="relative h-40 overflow-hidden rounded-t-lg">
+								<div className="absolute w-full p-4">
+									<div className="flex flex-row items-center">
+										<div className="text-2xl">
+											{isDataLoading && textToIcon[forest.mood]}
+										</div>
+										<h5 className="p-5 leading-loose text-2xl font-bold">
+											{forest.title}
+										</h5>
+									</div>
+									<div className="w-full justify-between space-x-2 items-center inline-flex mr-3 text-sm text-gray-900 dark:text-white">
+										<div className="flex flex-row space-x-2">
+											<img
+												className="w-10 h-10 object-cover"
+												// src={isDataLoading && forest.userInfo.profileImg}
+												src={
+													'https://png.pngtree.com/png-vector/20191115/ourmid/pngtree-beautiful-profile-line-vector-icon-png-image_1990469.jpg'
+												}
+												alt=""
+											/>
+											<div className="flex flex-col ">
+												<p className="w-20 text-sm dark:text-gray-400">
+													{isDataLoading && forest.userInfo.nickname}
+												</p>
+												<p className="text-sm dark:text-gray-400">
+													{isDataLoading && forest.userInfo.mbti}
+												</p>
+											</div>
+										</div>
+										<div className="justify-end flex flex-col">
+											<p className="mb-1">
+												조회수 {isDataLoading && forest.views}
+											</p>
+											<p className="self-end text-xs mb-1">
+												{isDataLoading && formatRelativeTime(forest.updatedAt)}
+											</p>
+										</div>
+									</div>
 								</div>
 							</div>
-							<div className="justify-end flex flex-col">
-								<p className="mb-1">조회수 {isDataLoading && forest.views}</p>
-								<p className="self-end text-xs mb-1">
-									{isDataLoading && formatRelativeTime(forest.updatedAt)}
-								</p>
+							<div className="p-10">
+								<Viewer initialValue={isDataLoading ? forest.content : ''} />
 							</div>
 						</div>
-					</div>
-
-					<div className="text-sm text-end absolute top-1 right-1 mt-4 me-4">
-						{isDataLoading && forest.userInfo._id == id && (
-							<>
-								<button
-									onClick={handleDelete}
-									className="ml-2 underline underline-offset-2 text-red-400"
-								>
-									삭제
-								</button>
-							</>
-						)}
-					</div>
+					)}
 				</div>
-
 				<div className="flex flex-col">
-					<div className="relative top-0 p-10">
-						{isDataLoading && <Viewer initialValue={forest.content} />}
-					</div>
-
 					<div className="m-5 flex flex-row justify-center space-x-2">
 						<HandThumbUpIcon className="w-10 h-10" />
 						<HandThumbDownIcon className="w-10 h-10" />
--- front/src/components/Daenamus/DaenamuTextEditor.jsx
+++ front/src/components/Daenamus/DaenamuTextEditor.jsx
@@ -17,11 +17,6 @@ const DaenamuTextEditor = () => {
 		setContent,
 	} = useStoryStore();
 
-	const isFormValid = useMemo(
-		() => title && content && mood,
-		[title, content, mood],
-	);
-
 	const handleSubmit = async () => {
 		try {
 			const response = await postApi('forest/senti-predict', { content });
@@ -42,22 +37,6 @@ const DaenamuTextEditor = () => {
 
 	const { handleImageUpload, loading } = useImageUpload();
 
-	const postForest = async (e) => {
-		e.preventDefault();
-
-		try {
-			const post = { title, content, mood };
-
-			const response = await postApi('forest', post);
-			console.log(response.data);
-			setTimeout(() => {
-				window.location.href = '/daenamus';
-			}, 50);
-		} catch (e) {
-			console.error(e);
-		}
-	};
-
 	return (
 		<>
 			<h3 className="font-semibold">제목</h3>
@@ -71,65 +50,53 @@ const DaenamuTextEditor = () => {
 				value={title}
 			/>
 			<h3 className="font-semibold">본문</h3>
-			<div className="mt-5">
-				<Editor
-					initialValue=" "
-					placeholder="내용을 입력하세요."
-					previewStyle="vertical"
-					previewHighlight={false}
-					height="400px"
-					initialEditType="wysiwyg"
-					language="ko-KR"
-					toolbarItems={[
-						// 툴바 옵션 설정
-						['bold', 'italic', 'strike'],
-						['hr', 'quote'],
-						['ul', 'ol', 'task', 'indent', 'outdent'],
-						['table', 'image', 'link'],
-					]}
-					ref={editorRef}
-					onChange={onChange}
-					hooks={{
-						addImageBlobHook: handleImageUpload,
-					}}
-				/>
-				{content?.length <= 16 && (
-					<p className="text-right text-red-400 text-sm mt-2">
-						10자 이상 입력해주세요.
-					</p>
-				)}
-			</div>
-
-			{loading && <div>이미지 업로드 중...</div>}
-			<div className="flex flex-col space-y-2">
-				<div className="justify-end flex flex-row space-x-2">
-					{mood && (
-						<div className="text-lg">
-							게시글 분석 결과 : {textToKorean[mood]}
-							{textToIcon[mood]}
-						</div>
+			<div className="text-editor">
+				<div className="mt-5">
+					<Editor
+						initialValue={content}
+						placeholder="내용을 입력하세요."
+						previewStyle="vertical"
+						previewHighlight={false}
+						height="400px"
+						initialEditType="wysiwyg"
+						language="ko-KR"
+						toolbarItems={[
+							// 툴바 옵션 설정
+							['bold', 'italic', 'strike'],
+							['hr', 'quote'],
+							['ul', 'ol', 'task', 'indent', 'outdent'],
+							['table', 'image', 'link'],
+						]}
+						ref={editorRef}
+						onChange={onChange}
+						hooks={{
+							addImageBlobHook: handleImageUpload,
+						}}
+					/>
+					{content?.length <= 16 && (
+						<p className="text-right text-red-400 text-sm mt-2">
+							10자 이상 입력해주세요.
+						</p>
 					)}
-					<button
-						onClick={handleSubmit}
-						disabled={content?.length <= 16}
-						className="w-40 h-8 bg-blue-700 disabled:bg-neutral-300 text-white font-medium rounded-md text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					>
-						감정 분석하기
-					</button>
 				</div>
 
-				<div className="justify-end flex flex-col">
-					<button
-						disabled={!isFormValid}
-						onClick={postForest}
-						type="button"
-						className="w-40 self-end bg-blue-700 disabled:bg-neutral-300 text-white font-medium rounded-md text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					>
-						작성 완료
-					</button>
-					{!isFormValid && (
-						<p className="self-end text-red-500 text-xs">빈 칸을 채워주세요.</p>
-					)}
+				{loading && <div>이미지 업로드 중...</div>}
+				<div className="flex flex-col space-y-2">
+					<div className="justify-end flex flex-row space-x-2">
+						{mood && (
+							<div className="text-lg">
+								게시글 분석 결과 : {textToKorean[mood]}
+								{textToIcon[mood]}
+							</div>
+						)}
+						<button
+							onClick={handleSubmit}
+							disabled={content?.length <= 16}
+							className="w-40 h-8 bg-blue-700 disabled:bg-neutral-300 text-white font-medium rounded-md text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+						>
+							감정 분석하기
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
